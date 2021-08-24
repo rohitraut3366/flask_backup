c210c10522ac9223977d5667650c654a0c756109
Rohit Raut <rohitraut3366@gmail.com>
2021-08-24 17:37:13 +0530

    mongodb controller and service updates

--- app/main/controllers/mongodb_controller.py
+++ app/main/controllers/mongodb_controller.py
@@ -1,4 +1,14 @@
-from flask_restplus import Resource, abort
+from flask_restplus import Resource
+
+from app.main.services.mongo_db_service import mongo_backup
 from app.main.utils.mongodb_dto import MongoDB
+from flask import request
 
 api = MongoDB.api
+
+
+@api.route("api/v1.0/mongodb")
+class MongoDB(Resource):
+    def post(self):
+        data = request.json
+        return mongo_backup(data)
--- app/main/services/mongo_db_service.py
+++ app/main/services/mongo_db_service.py
@@ -1,24 +1,29 @@
 """mongodump --db somedb --gzip --archive > dump_`date "+%Y-%m-%d"`.gz"""
 from datetime import datetime
 
-from app.main.cloud_object_storage.aws import aws_s3
-from app.main.cloud_object_storage.gcp import upload_gcp_bucket
-from app.main.services.client import os_client
-from werkzeug.exceptions import BadRequest, InternalServerError
+from werkzeug.exceptions import BadRequest
 
+from app.main.services.client import os_client
 from app.main.services.utilities import sftp_, cloud_selection
 
 
 def mongo_backup(data):
-    ssh = os_client(data)
-    bucket_file_name = str(datetime.utcnow()).replace(" ", "-") + ".gz"
+    ssh = os_client(data["os"])
 
-    date_time = "/tmp/" + data['database']["backup_file_prefix"] + bucket_file_name
+    bucket_file_name = data['database']["backup_file_prefix"] + str(datetime.utcnow()).replace(" ", "-") + ".gz"
+    date_time = "/tmp/" + bucket_file_name
+
     try:
         ssh_stdout, ssh_stderr = ssh.exec_command(
-            f"""sudo mongodump --db {data["database"]} --gzip --archive > /tmp/{datetime}""")[1:]
+            f"""sudo mongodump --db {data["database"]["mongo_db_name"]} --gzip --archive > {date_time}""")[1:]
+        print(f"""sudo mongodump --db {data["database"]["mongo_db_name"]} --gzip --archive > {date_time}""")
+
     except Exception as e:
-        raise BadRequest("Backup Failed!")
+        return {
+            "operation": "mongo db backup",
+            "std_error": ssh_stderr.read().decode()
+        }
+
     if sftp_(ssh, date_time):
         ssh.exec_command(f"r -rf /tmp/{date_time}")
     else:
